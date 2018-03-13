4372ae446d77a5227e5b01c376aee3806ba622b4
jermnelson <jermnelson@gmail.com>
2018-03-13 12:41:44 -0600

    added parent app for production

--- run.py
+++ run.py
@@ -1,7 +1,14 @@
 __author__ = "Jeremy Nelson"
 
-#from aristotle import app
+from werkzeug.serving import run_simple
+from werkzeug.wsgi import DispatcherMiddleware
+
 from app import app
+
+application = DispatcherMiddleware(
+    app,
+    {"/digital-cc": app }
+)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8048, debug=True)
